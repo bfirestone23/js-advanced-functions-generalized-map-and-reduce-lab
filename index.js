30cb94da3d13006eb2509327c150fec02d3e271f
Brian Firestone <bfirestone2339@gmail.com>
2021-03-28 15:54:01 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Add your functions here
+const map = (arr, fn) => {
+    let result = [];
+    for (let i = 0; i < arr.length; i++) {
+        result.push(fn(arr[i]));
+    }
+    return result;
+}
+
+const reduce = (arr, fn, int=0) => {
+    let accumulator = (!!int) ? int : arr[0];
+    let currentValue = (!!int) ? 0 : 1;
+
+    for (; currentValue < arr.length; currentValue++) {
+        accumulator = fn(arr[currentValue], accumulator)
+    }
+    return accumulator;
+}
